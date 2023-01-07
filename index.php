aafa4da7dde793cae32af0e19c292aaf9750f527
Sam Itman <si237@njit.edu>
2023-01-07 10:51:57 -0500

    typewriter effect

--- index.php
+++ index.php
@@ -35,7 +35,8 @@
 		<div class="row">
 		<div class="col-md-5 col-sm-3"></div>
 			<div class="col-md-7 col-sm-9">
-				<h1>Welcome to Sam Itman's Website!</h1>
+				<div id="typewriter-text"></div>
+
 			</div>
 		</div>
 	</div>
